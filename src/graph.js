96cb46b036d5cd897119c77d9b5e9f3be2e8881e
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-06-10 23:06:10 +0530

    Amended BFS function in graph.js
    Added ObjectOfArray function which creats an object of array's for the given array of array's

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,34 @@
-//Example 
+//Example
 // Pairs => [[from,to],[to,from]]
 // Source => from
-// To => to 
+// To => to
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
+const objectOfArray = function (context, element) {
+  const [key, value] = element;
+  if (!context[key]) {
+    context[key] = [];
+  }
+  context[key].push(value);
+  return context;
 };
 
-module.exports = {bfs};
+const bfs = function (pairs, source, target) {
+  const adjacency_list = pairs.reduce(objectOfArray, {});
+  let visited = [];
+  let queue = [source];
+  while (queue.length != 0) {
+    let visiting_node = queue.shift();
+    visited.push(visiting_node);
+    let unvisited = adjacency_list[visiting_node].filter(
+      (node) => !visited.includes(node) && !queue.includes(node)
+    );
+    queue.push(...unvisited);
+    if (visiting_node === target) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = { bfs };
