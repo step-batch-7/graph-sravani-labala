bc17d7b762ea6d624f0e697f34c8169ee48116f6
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-06-11 09:00:44 +0530

    Modified the breadth first search function

--- src/graph.js
+++ src/graph.js
@@ -18,15 +18,16 @@ const bfs = function (pairs, source, target) {
   let visited = [];
   let queue = [source];
   while (queue.length != 0) {
-    let visiting_node = queue.shift();
-    visited.push(visiting_node);
-    let unvisited = adjacency_list[visiting_node].filter(
+    let visitingNode = queue.shift();
+    let visitingList = adjacency_list[visitingNode] || [];
+    if (visitingList.includes(target)) {
+      return true;
+    }
+    visited.push(visitingNode);
+    let unvisited = visitingList.filter(
       (node) => !visited.includes(node) && !queue.includes(node)
     );
     queue.push(...unvisited);
-    if (visiting_node === target) {
-      return true;
-    }
   }
   return false;
 };
